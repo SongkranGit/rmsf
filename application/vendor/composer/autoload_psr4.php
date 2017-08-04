912c78b9921296966d3a59e03249caa39043d202
COMPANY\ss <ss@manaosoftware.com>
2017-08-04 13:13:32 +0700

    Added composer Image Intervention

--- application/vendor/composer/autoload_psr4.php
+++ application/vendor/composer/autoload_psr4.php
@@ -6,5 +6,8 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
+    'Psr\\Http\\Message\\' => array($vendorDir . '/psr/http-message/src'),
+    'Intervention\\Image\\' => array($vendorDir . '/intervention/image/src/Intervention/Image'),
+    'GuzzleHttp\\Psr7\\' => array($vendorDir . '/guzzlehttp/psr7/src'),
     'Facebook\\' => array($vendorDir . '/facebook/php-sdk-v4/src/Facebook'),
 );

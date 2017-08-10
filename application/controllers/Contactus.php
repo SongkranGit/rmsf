88e37f384d26541a8b4ab59c61402ce40d19f591
Songkran Sommit <bsongkran@hotmail.com>
2017-08-10 23:56:06 +0700

    added view contact form

--- application/controllers/Contactus.php
+++ application/controllers/Contactus.php
@@ -14,4 +14,75 @@ class Contactus extends Frontend_Controller
         return $this->load->view('frontend/contact_us');
     }
 
+    function create(){
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+            $response = array('success' => false, 'notes' => array());
+
+            dump("sfsd");
+
+            $this->validateForm();
+
+//            if ($this->form_validation->run()) {
+//                $data = array(
+//                    "full_name" => trim($this->input->post("full_name")),
+//                    "phone" => trim($this->input->post("phone")),
+//                    "email" => trim($this->input->post("email")),
+//                    "age" => intval($this->input->post("age")),
+//                    "education" => intval($this->input->post("education")),
+//                    "line_id" => $this->input->post("line_id"),
+//                    "note" => $this->input->post("note"),
+//                    "created_date" => Calendar::currentDateTime()
+//                );
+//
+//                if ($this->Contact_model->save($data)) {
+//                    $response['success'] = true;
+//                }
+//
+//                echo json_encode($response);
+//
+//                if ($response["success"] == true) {
+//                    $this->sendEmail($data);
+//                    $this->sendEmailToInterestedPerson($data);
+//                }
+//
+//            } else {
+//                foreach ($_POST as $key => $value) {
+//                    $result['notes'][$key] = form_error($key);
+//                }
+//            }
+        }
+    }
+
+    public function sendEmail($data)
+    {
+        require_once(APPPATH.'libraries/Phpmailer.php');
+        $mailer = new PHPMailer();
+
+        $mailer->CharSet = "utf-8";
+        $mailer->IsSMTP();
+        $mailer->IsHTML(true);
+        $mailer->SMTPDebug = 0;
+        $mailer->SMTPAuth = true;
+        $mailer->Host = "mail.modernsofttech.com"; // SMTP server
+        $mailer->Port = 25; // พอร์ท
+        $mailer->Username = "ss@modernsofttech.com"; // account SMTP
+        $mailer->Password = "liver167"; // รหัสผ่าน SMTP
+
+        $mailer->SetFrom(strip_tags($data["email"]), $data["full_name"]);
+        $mailer->AddAddress(strip_tags($this->data["settings"]["email"]), "recipient1"); // ผู้รับคนที่หนึ่ง
+
+        $mailer->Subject = "ระบบส่งเมล์อัตโนมัต Study Plus Center ";
+        $mailer->Body = $this->getResponseEmailMessage($data);
+
+        if(!$mailer->Send()) {
+            echo "Mailer Error: " . $mailer->ErrorInfo;
+        } else {
+            // echo "Message sent!  ".dirname(__FILE__)."/uploads/". $this->qr_code_name.".png";
+        }
+
+    }
+
+
+
 }
--- application/views/frontend/contact_us.php
+++ application/views/frontend/contact_us.php
@@ -1,12 +1,42 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Contact Us</title>
-</head>
-<body>
-
-<h1>Contact us </h1>
+<?php $this->load->view("includes/frontend/header"); ?>
 
 
-</body>
-</html>
+<div class="container" >
+    <div class="col-md-5 col-md-offset-3">
+        <div class="form-area" >
+            <form role="form" method="post" action="<?php base_url('contact/create')?>">
+                <br style="clear:both">
+                <h1 style="margin-bottom: 25px; text-align: center;">Contact Form</h1>
+                <div class="form-group">
+                    <input type="text" class="form-control" id="name" name="name" placeholder="Name" required>
+                </div>
+                <div class="form-group">
+                    <input type="text" class="form-control" id="email" name="email" placeholder="Email" required>
+                </div>
+                <div class="form-group">
+                    <input type="text" class="form-control" id="mobile" name="mobile" placeholder="Mobile Number" required>
+                </div>
+                <div class="form-group">
+                    <input type="text" class="form-control" id="subject" name="subject" placeholder="Subject" required>
+                </div>
+                <div class="form-group">
+                    <textarea class="form-control" type="textarea" id="message" placeholder="Message" maxlength="140" rows="7"></textarea>
+                </div>
+
+                <button type="submit" id="submit" name="submit" class="btn btn-primary pull-right">Submit Form</button>
+            </form>
+        </div>
+    </div>
+</div>
+
+<script type="text/javascript">
+
+    $(document).ready(function () {
+
+    });
+
+
+
+</script>
+
+<?php $this->load->view("includes/frontend/footer"); ?>
